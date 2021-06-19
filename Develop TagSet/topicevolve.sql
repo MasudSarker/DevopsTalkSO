65410bdde8f7b5356e2713c0202e59e443d7e84e
MasudSarker <mict.mbstu@gmail.com>
2021-06-19 17:31:28 +0600

    Add files via upload

--- Develop TagSet/topicevolve.sql
+++ Develop TagSet/topicevolve.sql
@@ -1,17 +1,13 @@
 
 select 
-datediff(month, @MinDate, Journal_Date) / 6 as Group1,
-dateadd(month, datediff(month, '19000101',MIN(Journal_Date)),'19000101') as PeriodStart,
-dateadd(day,-1,dateadd(month, 1+datediff(month, '19000101',MAX(Journal_Date)),'19000101')) as PeriodEnd,
-sum(Gross_88ths_value) as ValueTotal from Topicevolve t
-GROUP BY datediff(month, @MinDate, Journal_Date) / 6;
-
-select top 26 
-datediff(MONTH,'2015-01-01',FORMAT(CAST(CreationDate as date),'yyyy-MM-dd'))/3 as MonthName,
---FORMAT(CAST(CreationDate as date),'yyyy-MM-dd'),
-dateadd(month, datediff(month, '19000101',MIN(FORMAT(CAST(CreationDate as date),'yyyy-MM-dd'))),'19000101') as PeriodStart,
-dateadd(day,-1,dateadd(month, 1+datediff(month, '19000101',MAX(FORMAT(CAST(CreationDate as date),'yyyy-MM-dd'))),'19000101')) as PeriodEnd
- from Topicevolve 
- where FORMAT(CAST(CreationDate as date),'yyyy-MM-dd')>'2015-01-01'
-group by datediff(MONTH,'2015-01-01',FORMAT(CAST(CreationDate as date),'yyyy-MM-dd')) /3
-order by datediff(MONTH,'2015-01-01',FORMAT(CAST(CreationDate as date),'yyyy-MM-dd')) /3
+datediff(MONTH,'2015-01-01',FORMAT(CAST(te.CreationDate as date),'yyyy-MM-dd'))/6 as MonthName,
+dateadd(month, datediff(month, '19000101',MIN(FORMAT(CAST(te.CreationDate as date),'yyyy-MM-dd'))),'19000101') as PeriodStart,
+dateadd(day,-1,dateadd(month, 1+datediff(month, '19000101',MAX(FORMAT(CAST(te.CreationDate as date),'yyyy-MM-dd'))),'19000101')) as PeriodEnd,
+tm.CategoriesName,
+count(*) as Post
+ from Topicevolve te, TopicsModel tm
+ where te.Keywords=tm.keywords and
+ FORMAT(CAST(te.CreationDate as date),'yyyy-MM-dd')>'2015-01-01'
+group by datediff(MONTH,'2015-01-01',FORMAT(CAST(te.CreationDate as date),'yyyy-MM-dd')) /6
+,tm.CategoriesName
+order by datediff(MONTH,'2015-01-01',FORMAT(CAST(te.CreationDate as date),'yyyy-MM-dd')) /6
